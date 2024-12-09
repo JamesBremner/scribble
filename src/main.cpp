2050f102e38791b3d84857ebb54b24421c03f66d
JamesBremner <james@ravenspoint.com>
2024-12-09 12:26:04 -0500

    contour tracing

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,47 @@ public:
         : cStarterGUI(
               "Starter",
               {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
+          fdrawing(false)
     {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
+
+        fm.events().mouseMove(
+            [&](wex::sMouse &ms)
+            {
+                if (!ms.left)
+                {
+                    if (!fdrawing)
+                        return;
+                    fdrawing = false;
+                }
+                else
+                {
+                    fdrawing = true;
+                    myTrace.push_back(cxy(ms.x, ms.y));
+                }
+                fm.update();
+            });
 
         show();
         run();
     }
 
 private:
-    wex::label &lb;
+    bool fdrawing;
+    std::vector<cxy> myTrace;
+
+    void draw(wex::shapes &S)
+    {
+        S.fill();
+        S.color(0x00FF00);
+        for (int k = 0; k < myTrace.size(); k++)
+            S.circle(myTrace[k].x, myTrace[k].y, 10);
+        if (!fdrawing)
+        {
+            S.color(0x0000FF);
+            for (int k = 1; k < myTrace.size(); k++)
+                S.line(myTrace[k - 1], myTrace[k]);
+        }
+    }
 };
 
 main()
